42a017c6476384dbff8658fc5bd6ab6aec161443
mikehung <mikehung@synology.com>
2018-11-21 14:00:41 +0800

    Refine codeforces 1077B

--- codeforces/1077/B/sol.cc
+++ codeforces/1077/B/sol.cc
@@ -3,8 +3,7 @@
 using namespace std;
 
 const int MAX = 123;
-int n, a[MAX];
-vector<int> b;
+int n, a[MAX], ans;
 
 int main()
 {
@@ -14,14 +13,9 @@ int main()
     for (int i = 0; i < n; ++i)
         cin >> a[i];
     for (int i = 1; i < n-1; ++i) {
-        if (a[i-1] && !a[i] && a[i+1])
-            b.push_back(i);
-    }
-    int ans = b.size();
-    for (int i = 1; i < (int) b.size(); ++i) {
-        if (b[i] == b[i-1] + 2) {
-            ++i;
-            --ans;
+        if (a[i-1] && !a[i] && a[i+1]) {
+            ++ans;
+            a[i+1] = 0;
         }
     }
     cout << ans << '\n';
