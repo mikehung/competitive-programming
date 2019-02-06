c9220a259eb7a30dd3888e0cb14a1d39039be109
mikehung <slaotvaen@gmail.com>
2019-02-06 09:57:03 +0800

    Update codeforces 1108B

--- codeforces/1108/B/sol.cc
+++ codeforces/1108/B/sol.cc
@@ -6,20 +6,18 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n, mx = 1;
-    map<int, int> d;
+    int n, mx;
+    multiset<int> s;
     cin >> n;
     while (n--) {
         int x;
         cin >> x;
-        ++d[x];
-        mx = max(mx, x);
+        s.insert(x);
     }
+    mx = *prev(s.end());
     for (int i = 1; i <= mx; ++i) if (mx % i == 0) {
-        if (--d[i] == 0) {
-            d.erase(i);
-        }
+        s.erase(s.find(i));
     }
-    cout << mx << ' ' << d.rbegin()->first << '\n';
+    cout << mx << ' ' << *prev(s.end()) << '\n';
     return 0;
 }
