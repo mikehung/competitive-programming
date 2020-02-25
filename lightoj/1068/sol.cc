2c2a98cb354347414581f2692c089b9c68bce44e
Mike Hung <slaotvaen@gmail.com>
2020-02-25 21:58:20 +0800

    LightOJ-1068

--- lightoj/1068/sol.cc
+++ lightoj/1068/sol.cc
@@ -2,47 +2,39 @@
 
 using namespace std;
 
-const int MAX_LEN = 13, MAX_MOD = 90;  // the sum of digit will not exceed the MAX_MOD
-int dp[MAX_LEN][2][MAX_MOD][MAX_MOD];
-long long pw[MAX_LEN];
+int dp[35][2][100][100];
 
-int calc(int n, int k)
+int calc(int x, int K)
 {
-    string s = to_string(n);
-    int len = s.size();
-    for (int i = 0; i < MAX_LEN; ++i) for (int j = 0; j < 2; ++j) for (int k = 0; k < MAX_MOD; ++k) for (int l = 0; l < MAX_MOD; ++l)
-        dp[i][j][k][l] = 0;
+    string s = to_string(x);
+    int n = s.size();
+    memset(dp, 0, sizeof(dp));
+    dp[0][0][0][0] = 1;
 
-    dp[0][1][0][0] = 1;
-    for (int i = 0; i < len; ++i) for (int f = 0; f < 2; ++f) for (int mo_dig = 0; mo_dig < MAX_MOD; ++mo_dig) for (int mo_num = 0; mo_num < MAX_MOD; ++mo_num) {
-        if (!dp[i][f][mo_dig][mo_num]) continue;
-        for (int dig = 0; dig <= (f ? s[i]-'0' : 9); ++dig) {
-            int n_f = f && s[i]-'0' == dig;
-            int n_mo_dig = (mo_dig + dig) % k;
-            int n_mo_num = (mo_num + dig * pw[len-i-1]) % k;
-            dp[i+1][n_f][n_mo_dig][n_mo_num] += dp[i][f][mo_dig][mo_num];
+    for (int i = 0; i < n; ++i) for (int j = 0; j < 2; ++j) for (int k = 0; k < K; ++k) for (int l = 0; l < K; ++l) {
+        int lim = j ? 9 : s[i]-'0';
+        for (int d = 0; d < lim+1; ++d) {
+            dp[i+1][j || d < lim][(d+k)%K][(10*l+d)%K] += dp[i][j][k][l];
         }
     }
 
-    return dp[len][0][0][0] + dp[len][1][0][0];
+    return dp[n][0][0][0] + dp[n][1][0][0];
 }
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    pw[0] = 1;
-    for (int i = 1; i < 13; ++i)
-        pw[i] = pw[i-1] * 10;
-    int n, a, b, k, ans;
-    cin >> n;
-    for (int i = 1; i <= n; ++i) {
+    int T, a, b, k, an;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
         cin >> a >> b >> k;
-        if (k > MAX_MOD)
-            ans = 0;
-        else
-            ans = calc(b, k) - calc(a-1, k);
-        cout << "Case " << i << ": " << ans << '\n';
+        if (k >= 100) {
+            an = 0;
+        } else {
+            an = calc(b, k) - calc(a-1, k);
+        }
+        cout << "Case " << t << ": " << an << '\n';
     }
     return 0;
 }
